b5b0b9bf92dd686d89258dc675785a76e2f0b6c9
jejugasirim-blip <jeju.gaisirm@gmail.com>
2026-01-20 20:29:03 +0900

    first name mapping temp

--- functions/createLoanFromApplication.ts
+++ functions/createLoanFromApplication.ts
@@ -81,6 +81,7 @@ function mapLoanApplicationToLoan(application) {
   
   if (application.borrower_first_name || application.borrower_last_name) {
     individualInfo.push({
+      first_name: application.borrower_first_name,
       last_name: application.borrower_last_name,
       individual_email: application.borrower_email,
       individual_phone_number: application.borrower_phone,
